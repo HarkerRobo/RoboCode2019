704dc0a52a6347dbec8ee43c0a27467950d1357d
Angela Jia <42385351+22angiej@users.noreply.github.com>
2019-01-07 17:20:35 -0800

    Create Drivetrain subsystem

---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems;
+
+import harkerrobolib.subsystems.HSDrivetrain;
+
+public class Drivetrain extends HSDrivetrain {
+
+    @Override
+    protected void initDefaultCommand() {
+
+    }
+
+}
