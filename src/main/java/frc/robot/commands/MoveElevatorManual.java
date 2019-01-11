84f621df01a781f90b84bc68071c77b8a41d1ca1
Angela Jia <42385351+22angiej@users.noreply.github.com>
2019-01-10 19:34:08 -0800

    Begin work on MoveElevatorManual

--- src/main/java/frc/robot/commands/MoveElevatorManual.java
+++ src/main/java/frc/robot/commands/MoveElevatorManual.java
@@ -1,10 +1,18 @@
 package frc.robot.commands;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
 import frc.robot.OI;
 import frc.robot.subsystems.Elevator;
 import harkerrobolib.commands.IndefiniteCommand;
 import harkerrobolib.util.MathUtil;
 
+/**
+ * Moves elevator based on driver right joystick input.
+ * 
+ * @author  Angela Jia
+ * @version 1/
+ */
 public class MoveElevatorManual extends IndefiniteCommand {
 
     public MoveElevatorManual() {
@@ -13,13 +21,19 @@ public class MoveElevatorManual extends IndefiniteCommand {
 
     @Override
     public void initialize() {
-        
+        //Elevator.getInstance().getMaster().confi
     }
 
     @Override
     public void execute() {
-        //MathUtil.mapJoystickOutput(OI.getInstance().getDriverGamepad().getRightY(), )
-            
-        //Elevator.getInstance().getMaster().set(
+        OI oi = OI.getInstance();
+        double speed = MathUtil.mapJoystickOutput(OI.getInstance().getDriverGamepad().getRightY(), OI.DRIVER_DEADBAND);
+        Elevator.getInstance().getMaster().set(ControlMode.PercentOutput, speed);
+        if(Math.abs(speed) > oi.DRIVER_DEADBAND)
+        {
+            boolean isDown = speed < 0;
+            //boolean 
+        }
+        
     }
 }
