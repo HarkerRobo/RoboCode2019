84220e1cf0eb24b62daa517773a843397d2c2782
Finn Frankis <finn@teachmy.com>
2019-01-10 19:33:22 -0800

    Complete Wrist subsytem and basic manual wrist command

--- src/main/java/frc/robot/commands/MoveWristManual.java
+++ src/main/java/frc/robot/commands/MoveWristManual.java
@@ -1,7 +1,36 @@
 package frc.robot.commands;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+
+import frc.robot.OI;
+import frc.robot.RobotMap.Global;
+import frc.robot.subsystems.Wrist;
+import frc.robot.subsystems.Wrist.WristDirection;
 import harkerrobolib.commands.IndefiniteCommand;
 
+/**
+ * 
+ * @version 1/10/19
+ * @author Finn Frankis
+ */
 public class MoveWristManual extends IndefiniteCommand {
-    //public
+    public MoveWristManual () {
+        requires (Wrist.getInstance());
+    }
+    
+    public void initialize () {
+        Wrist.getInstance().getMasterTalon().configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, Global.PID_PRIMARY);
+    }
+
+    public void execute() {
+        double leftOperatorTrigger = OI.getInstance().getOperatorGamepad().getLeftTrigger();
+        double rightOperatorTrigger = OI.getInstance().getOperatorGamepad().getRightTrigger();
+
+        if (leftOperatorTrigger > rightOperatorTrigger) {
+            Wrist.getInstance().setWrist(leftOperatorTrigger, WristDirection.TO_BACK);
+        }
+        else {
+            Wrist.getInstance().setWrist(leftOperatorTrigger, WristDirection.TO_FRONT);
+        }
+    }
 }
