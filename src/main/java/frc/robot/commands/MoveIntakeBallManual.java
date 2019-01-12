7fca7434b7dd57b3faba3821713ccdb76dcc5b8e
Chirag Kaushik <spafindoople@gmail.com>
2019-01-11 18:52:42 -0800

    Complete MoveIntakeBallTime command

--- src/main/java/frc/robot/commands/MoveIntakeBallManual.java
+++ src/main/java/frc/robot/commands/MoveIntakeBallManual.java
@@ -6,8 +6,24 @@ import harkerrobolib.util.MathUtil;
 import frc.robot.subsystems.IntakeBall;
 
 
+/**
+ * Controls ball intake
+ * 
+ * @author Anirudh Kotamraju
+ * @version 1/11/2019
+ */
+
 public class MoveIntakeBallManual extends IndefiniteCommand {
+    
     public MoveIntakeBallManual() {
+        requires(IntakeBall.getInstance());
+    }
+
+    public void execute() {
+        double operatorLeftY = MathUtil.mapJoystickOutput(OI.getInstance().getDriverGamepad().getLeftY(), OI.OPERATOR_DEADBAND);
+        double driverLeftTrigger =  MathUtil.mapJoystickOutput(OI.getInstance().getOperatorGamepad().getLeftTrigger(), OI.DRIVER_DEADBAND);
+        
+        if(Math.abs(operatorLeftY) > 0) {
             IntakeBall.getInstance().setTalonOutput(operatorLeftY);            
         } else if(driverLeftTrigger > 0) {
             IntakeBall.getInstance().setTalonOutput(driverLeftTrigger);
