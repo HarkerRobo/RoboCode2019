740b8ce489f8592a76105ddca200cd16f885e84d
Angela Jia <42385351+22angiej@users.noreply.github.com>
2019-01-24 18:47:52 -0800

    Fix errors in ReachTarget

--- src/main/java/frc/robot/auto/automode/ReachTarget.java
+++ src/main/java/frc/robot/auto/automode/ReachTarget.java
@@ -2,6 +2,7 @@ package frc.robot.auto.automode;
 
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.commands.drivetrain.AlignWithLimelight;
+import frc.robot.commands.drivetrain.DriveToPosition;
 import frc.robot.commands.drivetrain.DriveWithVelocityTimed;
 import frc.robot.commands.hatchpanelintake.SetHatchPusherDirection;
 import frc.robot.subsystems.HatchPusher;
@@ -19,7 +20,7 @@ public class ReachTarget extends AutoMode {
     private static final double FLYING_VELOCITY= 4.20;
     private static final double CAMERA_HORIZONTAL_DISTANCE_FROM_TARGET = -1;
     private static final double CAMERA_X_DISTANCE_FROM_TARGET = 0.0;
-    private static final double BACK_UP_DISTANCE = -2.0
+    private static final double BACK_UP_DISTANCE = -2.0;
     // public Command getRightCommands(Location endLoc) {
         
     // }
@@ -27,17 +28,18 @@ public class ReachTarget extends AutoMode {
     public Command getLeftCommands(Location endLoc){
         return new SequentialCommandGroup(
             new DriveWithVelocityTimed(FLYING_TIME, FLYING_VELOCITY),
-            new AlignWithLimelight(CAMERA_HORIZONTAL_DISTANCE_FROM_TARGET, CAMERA_X_DISTANCE_FROM_TARGET).CAMERA_X_DISTANCE_FROM_TARGET),
+            new AlignWithLimelight(CAMERA_HORIZONTAL_DISTANCE_FROM_TARGET, CAMERA_X_DISTANCE_FROM_TARGET), 
             new SetHatchPusherDirection(HatchPusher.PushDirection.OUT),
             new  DriveToPosition(BACK_UP_DISTANCE)
         );                              
     }    
-} 
+ 
     public Command getRightCommands(Location endLoc){
         return new SequentialCommandGroup(
             new DriveWithVelocityTimed(FLYING_TIME, FLYING_VELOCITY),
-            new AlignWithLimelight(CAMERA_HORIZONTAL_DISTANCE_FROM_TARGET, CAMERA_X_DISTANCE_FROM_TARGET).CAMERA_X_DISTANCE_FROM_TARGET),
+            new AlignWithLimelight(CAMERA_HORIZONTAL_DISTANCE_FROM_TARGET, CAMERA_X_DISTANCE_FROM_TARGET),
             new SetHatchPusherDirection(HatchPusher.PushDirection.OUT),
             new  DriveToPosition(BACK_UP_DISTANCE)
         );                              
     }  
+}
