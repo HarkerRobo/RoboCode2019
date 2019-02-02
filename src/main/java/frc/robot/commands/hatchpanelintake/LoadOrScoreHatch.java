4e2c60c6f264e472ed31cdaa6f80b508dfe56804
Finn Frankis <finn@teachmy.com>
2019-02-02 11:28:43 -0800

    Complete command to choose between loading or scoring a hatch
    Co-authored-by: chiragzq <spafindoople@gmail.com>

---
+++ src/main/java/frc/robot/commands/hatchpanelintake/LoadOrScoreHatch.java
@@ -0,0 +1,39 @@
+package frc.robot.commands.hatchpanelintake;
+
+import edu.wpi.first.wpilibj.command.WaitCommand;
+import frc.robot.subsystems.HatchLatcher;
+import frc.robot.subsystems.HatchLatcher.ExtenderDirection;
+import frc.robot.subsystems.HatchLatcher.FlowerDirection;
+import harkerrobolib.auto.SequentialCommandGroup;
+
+/**
+ * Chooses between loading or scoring a hatch.
+ * 
+ * @author Finn Frankis
+ * @author Chirag Kaushik
+ * 
+ * @since 2/2/19
+ */
+public class LoadOrScoreHatch extends SequentialCommandGroup {
+    public enum ScoreState {
+        LOAD(FlowerDirection.OPEN), SCORE(FlowerDirection.CLOSED);
+        private FlowerDirection direction;
+        private ScoreState (FlowerDirection direction) {
+            this.direction = direction;
+        }
+
+        public FlowerDirection getFlowerDirection () {
+            return direction;
+        }
+    }
+    
+    public LoadOrScoreHatch (ScoreState scoreState) {
+        super(
+            new SetExtenderManual(ExtenderDirection.OUT),
+            HatchLatcher.getInstance().getExtenderState() == ExtenderDirection.IN ? 
+                new WaitCommand(HatchLatcher.EXTENDER_EXTEND_TIME) : new WaitCommand(0.0),
+            new SetFlowerManual(scoreState.getFlowerDirection())
+        );                  
+        requires(HatchLatcher.getInstance());                                                                
+    }
+}
