24a16b38dc34d2ce818bb7d96bf834220d7cb60d
Finn Frankis <finn@teachmy.com>
2019-02-08 19:30:30 -0800

    Create command to zero wrist
    Co-authored-by: Angela Jia <22angiej@students.harker.org>

---
+++ src/main/java/frc/robot/commands/wrist/ZeroWrist.java
@@ -0,0 +1,33 @@
+package frc.robot.commands.wrist;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.subsystems.Wrist;
+import frc.robot.subsystems.Wrist.WristDirection;
+
+/**
+ * Sets the wrist along the path that allows it
+ * to hit the limit switch at the front.
+ * 
+ * @author Angela Jia
+ * @since 2/8/19
+ */
+public class ZeroWrist extends Command {
+    private static final double ZERO_SPEED = 0.5;
+    
+    public ZeroWrist() {
+        requires(Wrist.getInstance());
+    }
+
+    @Override
+    public void execute() {
+        Wrist.getInstance().setWrist(ZERO_SPEED, WristDirection.TO_FRONT);
+    }
+
+    /**
+     * @return true if wrist has hit hard limit on front side
+     */
+	@Override
+	protected boolean isFinished() {
+		return Wrist.getInstance().getMasterTalon().getSensorCollection().isFwdLimitSwitchClosed();
+	}    
+}
