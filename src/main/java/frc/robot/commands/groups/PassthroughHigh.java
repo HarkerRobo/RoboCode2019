cf3dfecd99f9a09ae8f3848e1cbbb41c19b19a77
Finn Frankis <finn@teachmy.com>
2019-02-17 19:01:57 -0800

    Convert passthrough low/high to use conditional commands

--- src/main/java/frc/robot/commands/groups/PassthroughHigh.java
+++ src/main/java/frc/robot/commands/groups/PassthroughHigh.java
@@ -1,15 +1,12 @@
 package frc.robot.commands.groups;
 
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.Robot;
-import frc.robot.commands.arm.SetArmPosition;
 import frc.robot.commands.elevator.MoveElevatorMotionMagic;
 import frc.robot.commands.wrist.MoveWristMotionMagic;
-import frc.robot.subsystems.Arm.ArmDirection;
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Wrist;
-import harkerrobolib.auto.CommandGroupWrapper;
-import harkerrobolib.commands.PrintCommand;
+import frc.robot.util.ConditionalCommand;
 
 /**
  * Moves the wrist from one side of the elevator to the other safely.
@@ -19,48 +16,15 @@ import harkerrobolib.commands.PrintCommand;
  * @author Chirag Kaushik
  * @since 2/8/19
  */
-public class PassthroughHigh extends Command
-{    
-    private int desiredWristPos;
-    private Robot.Side currentSide;
-    private Robot.Side desiredSide;
-    private CommandGroupWrapper commandGroup;
-
+public class PassthroughHigh extends CommandGroup
+{   
     public PassthroughHigh(Robot.Side currentSide, int desiredWristPos) {
-        this.desiredWristPos = desiredWristPos;
-        this.desiredSide = Wrist.getInstance().getSide(desiredWristPos);
-        this.currentSide = currentSide;
-    }
-
-    @Override
-    public void initialize() {
-        System.out.println("starting a passthrough high from " + currentSide + " to " + desiredSide);
-        commandGroup = new CommandGroupWrapper();
-
-        if ((currentSide == Robot.Side.FRONT || currentSide == Robot.Side.AMBIGUOUS) 
-            && Elevator.getInstance().isBelow(Elevator.RAIL_POSITION)) {
-                System.out.println("we are below rail pos and currently front/ambig");
-            commandGroup.sequential(new PassthroughLow(currentSide, Wrist.MAX_BACKWARD_POSITION));
-        }
-        commandGroup.sequential(new PrintCommand("printcommand after passthrough low!"));
-        System.out.println("adding a elevatormotionmagic/movewristmotionmagic");
-        commandGroup.sequential(new MoveElevatorMotionMagic(Elevator.SAFE_HIGH_PASSTHROUGH_POSITION))
-                    .sequential(new MoveWristMotionMagic(desiredWristPos));
-        
-        commandGroup.sequential(new PrintCommand("printcommand command done"));
-        commandGroup.start();
-    }
-
-    @Override
-    public boolean isFinished() {
-        return commandGroup.isCompleted();       
-    } 
-    
-    @Override
-    public void interrupted()
-    {
-        System.out.println("Interrupted");
-        commandGroup.cancel(); 
+        addSequential(
+            new ConditionalCommand(() -> (currentSide == Robot.Side.FRONT || currentSide == Robot.Side.AMBIGUOUS) 
+                                            && Elevator.getInstance().isBelow(Elevator.RAIL_POSITION), 
+                                    new PassthroughLow(currentSide, Wrist.MAX_BACKWARD_POSITION)));
+        addSequential(new MoveElevatorMotionMagic(Elevator.SAFE_HIGH_PASSTHROUGH_POSITION));
+        addSequential(new MoveWristMotionMagic(desiredWristPos));
     }
 
     public void end () {
--- src/main/java/frc/robot/commands/groups/PassthroughLow.java
+++ src/main/java/frc/robot/commands/groups/PassthroughLow.java
@@ -1,6 +1,6 @@
 package frc.robot.commands.groups;
 
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.Robot;
 import frc.robot.commands.arm.SetArmPosition;
 import frc.robot.commands.elevator.MoveElevatorMotionMagic;
@@ -8,7 +8,8 @@ import frc.robot.commands.wrist.MoveWristMotionMagic;
 import frc.robot.subsystems.Arm.ArmDirection;
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Wrist;
-import harkerrobolib.auto.CommandGroupWrapper;
+import frc.robot.util.ConditionalCommand;
+import harkerrobolib.auto.SequentialCommandGroup;
 
 /**
  * Moves the wrist from one side of the elevator to the other safely.
@@ -18,49 +19,19 @@ import harkerrobolib.auto.CommandGroupWrapper;
  * @author Chirag Kaushik
  * @since 2/8/19
  */
-public class PassthroughLow extends Command
-{   
-    private int desiredWristPos;
-    private Robot.Side currentSide;
-    private Robot.Side desiredSide;
-    private CommandGroupWrapper commandGroup;
-
+public class PassthroughLow extends CommandGroup
+{ 
     public PassthroughLow(Robot.Side currentSide, int desiredWristPos) {
-        this.desiredWristPos = desiredWristPos;
-        this.desiredSide = Wrist.getInstance().getSide(desiredWristPos);
-        this.currentSide = currentSide;
-    }
+        Robot.Side desiredSide = Wrist.getInstance().getSide(desiredWristPos);
 
-    @Override
-    public void initialize() {
-        System.out.println("starting a passthrough low " + " from " + currentSide + " to " + desiredSide);
-        commandGroup = new CommandGroupWrapper();
-        System.out.println ("starting low passthrough");
-        if (desiredSide == Robot.Side.FRONT) {commandGroup.sequential(new SetArmPosition(ArmDirection.DOWN)); System.out.println("ac");}
-        if (currentSide != desiredSide) {
-            if ((currentSide == Robot.Side.FRONT || currentSide == Robot.Side.AMBIGUOUS) && 
-                    Elevator.getInstance().isAbove(Elevator.RAIL_POSITION)) {
-                    commandGroup.sequential (new PassthroughHigh (currentSide, Wrist.MAX_BACKWARD_POSITION));
-            System.out.println("aa");
-            }
-            commandGroup.sequential(new MoveElevatorMotionMagic(Elevator.SAFE_LOW_PASSTHROUGH_POSITION));
-            System.out.println("ab");
-        }
-        commandGroup.sequential(new MoveWristMotionMagic(desiredWristPos));
-        System.out.println("ad");
-
-        commandGroup.start();
-    }
-
-    @Override
-    public boolean isFinished() {
-        return commandGroup.isCompleted();       
-    } 
-    
-    @Override
-    public void interrupted()
-    {
-        commandGroup.cancel(); 
+        addSequential(new ConditionalCommand(() -> desiredSide == Robot.Side.FRONT, new SetArmPosition(ArmDirection.DOWN)));
+        addSequential(new ConditionalCommand(() -> currentSide != desiredSide, new SequentialCommandGroup(
+            new ConditionalCommand(
+                () -> (currentSide == Robot.Side.FRONT || currentSide == Robot.Side.AMBIGUOUS) && 
+                    Elevator.getInstance().isAbove(Elevator.RAIL_POSITION), 
+                new PassthroughHigh (currentSide, Wrist.MAX_BACKWARD_POSITION)), 
+            new MoveElevatorMotionMagic(Elevator.SAFE_LOW_PASSTHROUGH_POSITION))));
+        addSequential(new MoveWristMotionMagic(desiredWristPos));
     }
 
     public void end () {
