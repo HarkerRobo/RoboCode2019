6f931997bb5e954973cc5b0413d636adfe5c40b4
Finn Frankis <finn@teachmy.com>
2019-02-19 16:27:52 -0800

    Change Pair class to better follow conventions

--- src/main/java/frc/robot/util/Pair.java
+++ src/main/java/frc/robot/util/Pair.java
@@ -6,20 +6,20 @@ package frc.robot.util;
  * @author Finn Frankis
  * @since 2/2/19
  */
-public class Pair <K, V> {
-    private K first;
-    private V second;
+public class Pair <T1, T2> {
+    private T1 first;
+    private T2 second;
 
-    public Pair (K first, V second) {
+    public Pair (T1 first, T2 second) {
         this.first = first;
         this.second = second;
     }
 
-    public K getFirst () {
+    public T1 getFirst () {
         return first;
     }
 
-    public V getSecond () {
+    public T2 getSecond () {
         return second;
     }
 }
