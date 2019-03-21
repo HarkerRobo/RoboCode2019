d559a08b1ab75e984253c978e462e25a2bc9e21e
20JatinK <20finnf@students.harker.org>
2019-03-21 14:48:28 -0700

    Modify elevator/wrist constants to be consistent across practice/comp
    Co-authored-by: ange1a-j14 <22angiej@students.harker.org>

--- src/main/java/frc/robot/subsystems/Wrist.java
+++ src/main/java/frc/robot/subsystems/Wrist.java
@@ -112,7 +112,7 @@ public class Wrist extends Subsystem {
          SCORING_POSITION_FRONT_CARGO_SHIP = PARALLEL_FRONT - 10;
          SCORING_POSITION_BACK_CARGO_SHIP = 188;
 
-         ARBITRARY_FF = 0.002;// 17;
+         ARBITRARY_FF = 0.025; //0.002;// 17;
 
          ANGLE_INTAKE = 180;
          HATCH_INTAKING_POSITION = PARALLEL_BACK;
