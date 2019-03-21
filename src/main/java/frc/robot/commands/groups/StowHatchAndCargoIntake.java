8367f618a59d4c10e084cd79a08684d94e4e8090
20JatinK <20finnf@students.harker.org>
2019-03-21 14:48:04 -0700

    Add delay before entering defense mode
    Co-authored-by: ange1a-j14 <22angiej@students.harker.org>

--- src/main/java/frc/robot/commands/groups/StowHatchAndCargoIntake.java
+++ src/main/java/frc/robot/commands/groups/StowHatchAndCargoIntake.java
@@ -1,5 +1,6 @@
 package frc.robot.commands.groups;
 
+import edu.wpi.first.wpilibj.command.WaitCommand;
 import frc.robot.Robot;
 import frc.robot.Robot.Side;
 import frc.robot.commands.arm.SetArmPosition;
@@ -24,6 +25,7 @@ import harkerrobolib.commands.CallMethodCommand;
  */
 public class StowHatchAndCargoIntake extends SequentialCommandGroup {
 
+      public static final double ARM_WAIT_TIME = 0.5;
    public StowHatchAndCargoIntake() {
       super(new CallMethodCommand(() -> Robot.log("Entering defense mode.")),
             new SetExtenderState(ExtenderDirection.IN),
@@ -31,7 +33,7 @@ public class StowHatchAndCargoIntake extends SequentialCommandGroup {
                   () -> (Wrist.getInstance().getCurrentSide() == Side.FRONT
                         && !Elevator.getInstance().isAbove(Elevator.RAIL_POSITION)),
                   new SetScoringPosition(Location.PARALLEL_FRONT), new SetScoringPosition(Location.PARALLEL_BACK)),
-            new MoveWristMotionMagic(Wrist.MID_POSITION), new SetArmPosition(ArmDirection.UP));
+            new MoveWristMotionMagic(Wrist.MID_POSITION), new WaitCommand(ARM_WAIT_TIME), new SetArmPosition(ArmDirection.UP));
    }
 }
 
